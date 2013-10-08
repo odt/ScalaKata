8f5717438a1aa67aeefe2593b88c00a231c73287
akausagi <akausagi@usagee.co.jp>
2013-10-09 02:35:24 +0900

    whileによる実装

--- 13_10_08_KarateChop/akausagi/src/main/scala/jp/co/usagee/akausagi/chop/KarateChop.scala
+++ 13_10_08_KarateChop/akausagi/src/main/scala/jp/co/usagee/akausagi/chop/KarateChop.scala
@@ -8,8 +8,24 @@ package jp.co.usagee.akausagi.chop
  * To change this template use File | Settings | File Templates.
  */
 object KarateChop {
-  def chop(target: Int, sorted_list: List[Int], low_index: Int = 0): Int = {
-    return sorted_list.indexOf(target)
+  def chop(target: Int, sorted_list: List[Int]): Int = {
+    if(sorted_list.length == 0) return -1
+    var left_index = 0
+    var right_index = sorted_list.length - 1
+
+    while(left_index <= right_index){
+      var middle_index = (right_index+left_index)/2
+      var middle_value = sorted_list(middle_index)
+      middle_value match {
+        case x if x == target => return middle_index
+        //左側にターゲットがある場合
+        case x if target < x => right_index = middle_index - 1
+        //右側にターゲットがある場合
+        case x if target > x => left_index = middle_index + 1
+        case _ => return -1
+      }
+    }
+    return -1
   }
 
 }
