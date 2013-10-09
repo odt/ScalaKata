aba513982f7c8793233c941a7dc1569dc92aa6a1
TANIGUCHI Kohei <a.d.xvii.kal.mai@gmail.com>
2013-10-09 20:18:47 +0900

    上限を使った多相メソッドに変更

--- 13_10_08_KarateChop/cohei/src/main/scala/KarateChop.scala
+++ 13_10_08_KarateChop/cohei/src/main/scala/KarateChop.scala
@@ -4,17 +4,17 @@ import scala.annotation.tailrec
 
 object KarateChop {
   def main(args: Array[String]) = {
-    println(binSearch(4, Vector()))
+    println(binSearch(4, List(1,2,4,8)))
   }
 
-  def binSearch(y: Int, xs: Seq[Int]): Int = {
+  def binSearch[A <% Ordered[A]](y: A, xs: Seq[A]): Int = {
     toResult(binSearchHelper(0, xs.length - 1, y, xs))
   }
 
-  private def toResult(x: Option[Int]):Int = x getOrElse -1
+  private def toResult(x: Option[Int]):Int = x.getOrElse(-1)
 
   @tailrec
-  private def binSearchHelper(min: Int, max: Int, y: Int, xs: Seq[Int]): Option[Int] = {
+  private def binSearchHelper[A <% Ordered[A]](min: Int, max: Int, y: A, xs: Seq[A]): Option[Int] = {
     if (min > max) return None
 
     val mid = (min + max) / 2
