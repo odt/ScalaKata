fe99b03c603d999eb14df3cdd14063b3a38562af
tomykaira <tomykaira@gmail.com>
2013-10-13 12:15:28 +0900

    Create command line interface

--- 13_10_11_participants/tomykaira/participants/source/app.d
+++ 13_10_11_participants/tomykaira/participants/source/app.d
@@ -1,6 +1,14 @@
+import std.conv;
 import std.stdio;
 
+import participants.participants;
+import participants.date;
+
 void main()
-{ 
-	writeln("Edit source/app.d to start your project.");
+{
+    string input, buf;
+    while ((buf = readln()) !is null)
+        input ~= buf;
+    auto participants = new Participants(to!string(input));
+    writeln(participants.onDate(new Date(2013, 10, 9)));
 }
