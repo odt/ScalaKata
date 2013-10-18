3d09a677b3f0f20f2ebf4fc71f69b98823ac2bf3
TANIGUCHI Kohei <a.d.xvii.kal.mai@gmail.com>
2013-10-18 09:30:02 +0900

    temp 2

--- 13_10_15_data_munging/cohei/src/main/scala/MungingData.scala
+++ 13_10_15_data_munging/cohei/src/main/scala/MungingData.scala
@@ -4,9 +4,13 @@ import scala.io._
 object MungingData {
   def main(args: Array[String]) = {
     val source = Source.fromFile("weather.dat")
-    source.getLines foreach println
+    val lines = source.getLines.toList.drop(8)
+
   }
 
+  def words(s: String) = {
+    s.split(' ')
+  }
   // StringLike#split
 
 
