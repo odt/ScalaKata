7b8d5b43622ec04222e7ada1fd516fb5b092e7b2
joker1007 <kakyoin.hierophant@gmail.com>
2013-10-25 22:23:51 +0900

    initial fake impl

---
+++ 13_10_25_java_bizarre_version/joker1007/java_bizarre_version/src/main/scala/io/github/joker1007/JavaVersion.scala
@@ -0,0 +1,8 @@
+package io.github.joker1007
+
+
+object JavaVersion {
+  def validate(str : String) : Boolean = {
+    true
+  }
+}
--- 13_10_25_java_bizarre_version/joker1007/java_bizarre_version/src/test/scala/JavaVersionSpec.scala
+++ 13_10_25_java_bizarre_version/joker1007/java_bizarre_version/src/test/scala/JavaVersionSpec.scala
@@ -1,10 +1,10 @@
+import io.github.joker1007.JavaVersion
 import org.scalatest.FlatSpec
 import org.scalatest.matchers.ShouldMatchers
 
-import _root_.JavaVersion
 
 class JavaVersionSpec extends FlatSpec with ShouldMatchers {
   "JavaVersion.validate(str)" should "return true if str is valid java version string" in {
-    JavaVersion.validate(JDK7u40) should be (true)
+    JavaVersion.validate("JDK7u40") should be (true)
   }
 }
