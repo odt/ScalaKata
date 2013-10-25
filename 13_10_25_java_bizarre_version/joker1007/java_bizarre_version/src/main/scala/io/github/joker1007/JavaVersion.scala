8e5ec53d1d4fb1e1e1d389b45e8d27d0471e127f
joker1007 <kakyoin.hierophant@gmail.com>
2013-10-25 22:48:53 +0900

    Add various version test case

--- 13_10_25_java_bizarre_version/joker1007/java_bizarre_version/src/main/scala/io/github/joker1007/JavaVersion.scala
+++ 13_10_25_java_bizarre_version/joker1007/java_bizarre_version/src/main/scala/io/github/joker1007/JavaVersion.scala
@@ -2,7 +2,7 @@ package io.github.joker1007
 
 
 object JavaVersion {
-  val versionRegex = """JDK\du\d\d""".r
+  val versionRegex = """^JDK([1-9])\d*u([1-9])\d*$""".r
 
   def validate(str : String) : Boolean = {
     versionRegex findFirstIn str match {
--- 13_10_25_java_bizarre_version/joker1007/java_bizarre_version/src/test/scala/JavaVersionSpec.scala
+++ 13_10_25_java_bizarre_version/joker1007/java_bizarre_version/src/test/scala/JavaVersionSpec.scala
@@ -2,13 +2,26 @@ import io.github.joker1007.JavaVersion
 import org.scalatest.FlatSpec
 import org.scalatest.matchers.ShouldMatchers
 
-
-class JavaVersionSpec extends FlatSpec with ShouldMatchers {
-  "JavaVersion.validate(str)" should "return true if str is valid java version string" in {
-    JavaVersion.validate("JDK7u40") should be (true)
+trait VersionBehavior { this: FlatSpec with ShouldMatchers =>
+  def validVersion(str: String) {
+    it should "return true" in {
+      JavaVersion.validate(str) should be (true)
+    }
   }
 
-  "JavaVersion.validate(str)" should "return false if str is invalid java version string" in {
-    JavaVersion.validate("JDK7u9x") should be (false)
+  def invalidVersion(str: String) {
+    it should "return false" in {
+      JavaVersion.validate(str) should be (false)
+    }
   }
+}
+
+class JavaVersionSpec extends FlatSpec with ShouldMatchers with VersionBehavior {
+  "JDK7u40"  should behave like validVersion("JDK7u40")
+  "JDK10u40" should behave like validVersion("JDK10u40")
+  "JDK10u40" should behave like invalidVersion("JDK0u40")
+  "JDK7u9x"  should behave like invalidVersion("JDK7u9x")
+  "JDK07u9x" should behave like invalidVersion("JDK07u9x")
+  "JDK7u0"   should behave like invalidVersion("JDK7u0")
+  "JDK7u1"   should behave like validVersion("JDK7u1")
 }
