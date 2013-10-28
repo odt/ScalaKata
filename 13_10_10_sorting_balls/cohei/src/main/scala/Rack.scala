1c17ec40f8d42344a7c4cc56fc61b78423e0c64c
TANIGUCHI Kohei <a.d.xvii.kal.mai@gmail.com>
2013-10-28 14:59:14 +0900

    見た目の調整

--- 13_10_10_sorting_balls/cohei/src/main/scala/Rack.scala
+++ 13_10_10_sorting_balls/cohei/src/main/scala/Rack.scala
@@ -1,16 +1,14 @@
 import scalaz._
 
 case class Rack[A: Order](balls: Seq[A] = Seq()) {
-  def add(ball: A): Rack[A] = {
-    copy(insert(ball, balls))
-  }
+  def add(ball: A): Rack[A] = copy(insert(ball, balls))
 
   private def insert(y: A, xs: Seq[A]): Seq[A] = {
     xs match {
-      case Seq() => Seq(y)
+      case Seq()           => Seq(y)
       case Seq(x, xs @ _*) => Order[A].order(y,x) match {
         case Ordering.GT => x +: insert(y, xs)
-        case _  => y +: x +: xs
+        case _           => y +: x +: xs
       }
     }
   }
